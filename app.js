de469de4e0602e629036d1261aee583afc25a8dd
javi-dh <javier@digitalhouse.com>
2021-12-13 19:46:12 -0500

    agregue el env.PORT

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const app = express();
 
-app.listen(3000, () => console.log('Listening on port 3000'));
+app.listen(process.env.PORT || 3000, () => console.log('Listening on port 3000'));
 
 app.use(express.static(__dirname + '/public'));
 
